c629525f3dc3f14e38a956485f482c79276c2b4e
DanielMac1212 <94565662+DanielMac1212@users.noreply.github.com>
2026-03-13 17:42:51 +0800

    Update extract.py

--- extract.py
+++ extract.py
@@ -133,7 +133,7 @@ def main():
     if pd.notnull(tax_balance):
         weeks_tax_remaining = round(tax_balance / 90, 2)
     
-    df = df.where(pd.notnull(df), None)
+    df = df.replace({float("nan"): None})
     
     output = {
         "payslips": df.to_dict(orient="records"),
